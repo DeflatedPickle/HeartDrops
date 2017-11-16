820fbb6b1798b9531416d193dcb278281b473ae6
DeflatedPickle <DeflatedPickle@gmail.com>
2017-11-16 21:55:02 +0000

    Fixed references

--- src/main/java/com/deflatedpickle/heartdrops/Reference.java
+++ src/main/java/com/deflatedpickle/heartdrops/Reference.java
@@ -1,12 +1,12 @@
 package com.deflatedpickle.heartdrops;
 
-class Reference {
-    static final String MOD_ID = "heartdrops";
-    static final String NAME = "Heart Drops";
+public class Reference {
+    public static final String MOD_ID = "heartdrops";
+    public static final String NAME = "Heart Drops";
     // Versions follow this format: MCVERSION-MAJORMOD.MAJORAPI.MINOR.PATCH.
-    static final String VERSION = "1.12.2-1.0.0.0";
-    static final String ACCEPTED_VERSIONS = "[1.12.2]";
+    public static final String VERSION = "1.12.2-1.0.0.0";
+    public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 
-    static final String CLIENT_PROXY_CLASS = "com.deflatedpickle.heartdrops.proxy.ClientProxy";
-    static final String SERVER_PROXY_CLASS = "com.deflatedpickle.heartdrops.proxy.ServerProxy";
+    public static final String CLIENT_PROXY_CLASS = "com.deflatedpickle.heartdrops.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "com.deflatedpickle.heartdrops.proxy.ServerProxy";
 }
