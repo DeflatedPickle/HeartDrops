c8e16270d1016cfe605077522476b2386c13eefd
DeflatedPickle <DeflatedPickle@gmail.com>
2017-11-17 22:15:54 +0000

    Updated accepted versions

--- src/main/java/com/deflatedpickle/heartdrops/Reference.java
+++ src/main/java/com/deflatedpickle/heartdrops/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
     public static final String NAME = "Heart Drops";
     // Versions follow this format: MCVERSION-MAJORMOD.MAJORAPI.MINOR.PATCH.
     public static final String VERSION = "1.12.2-1.4.0.0";
-    public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+    public static final String ACCEPTED_VERSIONS = "[1.12.1, 1.12.2]";
 
     public static final String CLIENT_PROXY_CLASS = "com.deflatedpickle.heartdrops.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.deflatedpickle.heartdrops.proxy.ServerProxy";
