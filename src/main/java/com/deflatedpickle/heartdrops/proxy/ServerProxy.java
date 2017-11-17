c77c3d7cfe0bd85784038cde37b8a1aa1e056693
DeflatedPickle <DeflatedPickle@gmail.com>
2017-11-17 16:48:59 +0000

    Updated version

--- src/main/java/com/deflatedpickle/heartdrops/proxy/ServerProxy.java
+++ src/main/java/com/deflatedpickle/heartdrops/proxy/ServerProxy.java
@@ -3,7 +3,6 @@ package com.deflatedpickle.heartdrops.proxy;
 import com.deflatedpickle.heartdrops.events.FMLEventHandler;
 import com.deflatedpickle.heartdrops.init.ModItems;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 
 public class ServerProxy implements CommonProxy{
     @Override
@@ -13,7 +12,7 @@ public class ServerProxy implements CommonProxy{
 
     @Override
     public void init() {
-        FMLCommonHandler.instance().bus().register(new FMLEventHandler());
+        // FMLCommonHandler.instance().bus().register(new FMLEventHandler());
         MinecraftForge.EVENT_BUS.register(new FMLEventHandler());
     }
 }
