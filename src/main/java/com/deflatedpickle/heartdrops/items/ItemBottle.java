0e1f36781d58e8f183a6eb1a73e334b8f8bca443
DeflatedPickle <DeflatedPickle@gmail.com>
2017-11-20 21:21:55 +0000

    Added a capability

--- src/main/java/com/deflatedpickle/heartdrops/items/ItemBottle.java
+++ src/main/java/com/deflatedpickle/heartdrops/items/ItemBottle.java
@@ -4,10 +4,21 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.capabilities.ICapabilitySerializable;
+import net.minecraftforge.items.ItemStackHandler;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import static net.minecraftforge.items.CapabilityItemHandler.ITEM_HANDLER_CAPABILITY;
 
 public class ItemBottle extends ItemBase {
     public ItemBottle() {
@@ -45,5 +56,37 @@ public class ItemBottle extends ItemBase {
     @Override
     public int getMaxItemUseDuration(ItemStack stack) {
         return 32;
+    }
+
+    @Override
+    public ICapabilityProvider initCapabilities(ItemStack stack, @Nullable NBTTagCompound nbt) {
+        return new CapabilityHeartJar();
+    }
+
+    public final class CapabilityHeartJar implements ICapabilitySerializable<NBTTagCompound> {
+
+        private final ItemStackHandler inventory = new ItemStackHandler(12);
+
+        @Override
+        public boolean hasCapability(@Nonnull Capability<?> capability, @Nullable EnumFacing facing) {
+            return ITEM_HANDLER_CAPABILITY.equals(capability);
+        }
+
+        @Nullable
+        @Override
+        public <T> T getCapability(@Nonnull Capability<T> capability, @Nullable EnumFacing facing) {
+            return hasCapability(capability, facing) ? ITEM_HANDLER_CAPABILITY.cast(inventory) : null;
+        }
+
+        @Override
+        public NBTTagCompound serializeNBT() {
+            return inventory.serializeNBT();
+        }
+
+        @Override
+        public void deserializeNBT(NBTTagCompound nbt) {
+            inventory.deserializeNBT(nbt);
+        }
+
     }
 }
