06513227953a899216c7c6391eaab991561a831f
DeflatedPickle <DeflatedPickle@gmail.com>
2018-03-24 23:23:56 +0000

    Converted Reference class

---
+++ src/main/scala/com/deflatedpickle/heartdrops/Reference.scala
@@ -0,0 +1,14 @@
+package com.deflatedpickle.heartdrops
+
+object Reference {
+  val MOD_ID = "heartdrops"
+  val NAME = "Heart Drops"
+  // Versions follow this format: MCVERSION-MAJORMOD.MAJORAPI.MINOR.PATCH.
+  val VERSION = "1.12.2-2.0.0.0"
+  val ACCEPTED_VERSIONS = "[1.12.1, 1.12.2]"
+
+  val CLIENT_PROXY_CLASS = "com.deflatedpickle.heartdrops.proxy.ClientProxy"
+  val SERVER_PROXY_CLASS = "com.deflatedpickle.heartdrops.proxy.ServerProxy"
+
+  val CONFIG_GENERAL = "heartdrops"
+}
