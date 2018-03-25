6045b72d19011bfbbfa4d1d4eaa68700cde99039
DeflatedPickle <DeflatedPickle@gmail.com>
2018-03-25 00:29:04 +0000

    Revert "Converted Reference class"
    This reverts commit 06513227953a899216c7c6391eaab991561a831f.

---
+++ src/main/java/com/deflatedpickle/heartdrops/Reference.java
@@ -0,0 +1,14 @@
+package com.deflatedpickle.heartdrops;
+
+public class Reference {
+    public static final String MOD_ID = "heartdrops";
+    public static final String NAME = "Heart Drops";
+    // Versions follow this format: MCVERSION-MAJORMOD.MAJORAPI.MINOR.PATCH.
+    public static final String VERSION = "1.12.2-1.0.8.1";
+    public static final String ACCEPTED_VERSIONS = "[1.12.1, 1.12.2]";
+
+    public static final String CLIENT_PROXY_CLASS = "com.deflatedpickle.heartdrops.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "com.deflatedpickle.heartdrops.proxy.ServerProxy";
+
+    public static final String CONFIG_GENERAL = "heartdrops";
+}
