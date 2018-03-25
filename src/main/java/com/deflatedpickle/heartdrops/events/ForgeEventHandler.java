7651e1cdb9d04adb3d2ec6530f11375b14bf5f8c
DeflatedPickle <DeflatedPickle@gmail.com>
2018-03-25 04:11:59 +0100

    Cleaned up the ForgeEventHandler class

--- src/main/java/com/deflatedpickle/heartdrops/events/ForgeEventHandler.java
+++ src/main/java/com/deflatedpickle/heartdrops/events/ForgeEventHandler.java
@@ -19,22 +19,35 @@ public class ForgeEventHandler {
         if (!entity.world.isRemote) {
             if (event.getSource().getImmediateSource() instanceof EntityPlayer) {
                 player = (EntityPlayer) event.getSource().getImmediateSource();
-            } else if(event.getSource().getTrueSource() instanceof EntityPlayer) {
+            }
+            else if(event.getSource().getTrueSource() instanceof EntityPlayer) {
                 player = (EntityPlayer) event.getSource().getTrueSource();
             }
 
             if (player != null) {
                 if (GeneralConfig.dropWhen == GeneralConfig.When.HURT) {
-                    if (player.getHealth() < player.getMaxHealth()) { spawnItems = true; }
-                } else if (GeneralConfig.dropWhen == GeneralConfig.When.ALWAYS) { spawnItems = true; }
+                    if (player.getHealth() < player.getMaxHealth()) {
+                        spawnItems = true;
+                    }
+                }
+                else if (GeneralConfig.dropWhen == GeneralConfig.When.ALWAYS) {
+                    spawnItems = true;
+                }
 
                 if (!GeneralConfig.dropHardcore) {
-                    if (player.world.getMinecraftServer().isHardcore()) { return; }
+                    if (player.world.getMinecraftServer().isHardcore()) {
+                        return;
+                    }
                 }
             }
 
-            if (!spawnItems) { return; }
-            if (!player.world.getDifficulty().name().equals(GeneralConfig.dropDifficulty.toString()) && !GeneralConfig.dropDifficulty.toString().equals("ALL")) { return; }
+            if (!spawnItems) {
+                return;
+            }
+
+            if (!player.world.getDifficulty().name().equals(GeneralConfig.dropDifficulty.toString()) && !GeneralConfig.dropDifficulty.toString().equals("ALL")) {
+                return;
+            }
 
             EntityItem item = new EntityItem(entity.world, entity.posX, entity.posY, entity.posZ, new ItemStack(ModItems.heart, 1));
             entity.world.spawnEntity(item);
