665dc28af074cff3481d96dd5cf2f9e9bc240fc7
DeflatedPickle <DeflatedPickle@gmail.com>
2018-04-06 02:50:43 +0100

    Added a package info file

---
+++ src/main/java/com/deflatedpickle/heartdrops/api/package-info.java
@@ -0,0 +1,5 @@
+@API(owner = Reference.NAME, apiVersion = Reference.VERSION, provides = "HeartDrops|API")
+package com.deflatedpickle.heartdrops.api;
+
+import com.deflatedpickle.heartdrops.Reference;
+import net.minecraftforge.fml.common.API;
