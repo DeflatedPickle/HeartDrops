d15d286eeb88ff9f26ba2ec00a6b0f5c2a435d5f
DeflatedPickle <DeflatedPickle@gmail.com>
2018-04-06 05:03:10 +0100

    Updated version

--- src/main/java/com/deflatedpickle/heartdrops/Reference.java
+++ src/main/java/com/deflatedpickle/heartdrops/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
     public static final String MOD_ID = "heartdrops";
     public static final String NAME = "HeartDrops";
     // Versions follow this format: MCVERSION-MAJORMOD.MAJORAPI.MINOR.PATCH.
-    public static final String VERSION = "1.12.2-2.0.1.0";
+    public static final String VERSION = "1.12.2-2.0.3.0";
     public static final String ACCEPTED_VERSIONS = "[1.12.1, 1.12.2]";
 
     public static final String CONFIG_GENERAL = "heartdrops";
